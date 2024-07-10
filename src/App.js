a23b576f862674c0cddb2d81cae94614c787c235
RBC <itt04103@gmail.com>
2024-07-10 09:50:03 +0300

    second font end commit

--- src/App.js
+++ src/App.js
@@ -1,73 +1,63 @@
-import FComponent from "./components/FComponent";
-import CComponent from "./components/CComponent";
-import State from "./components/State";
-import Counter from "./components/Counter";
-import FunctionClick from "./components/FunctionClick";
-import ClassClick from "./components/ClassClick";
-import Style from "./components/Style";
-import './AppStyle.css';
-import styles from './App.module.css';
-import Form from "./components/Form";
-import Fragment from "./components/Fragment";
-import Table from "./components/Table";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import PT from './pages/PT';
+import Login from './pages/Login';
+import ContactUs from './pages/ContactUs';
+import Review from './pages/Review';
+import { Index } from './dashbroad/Index';
+import ManageStudent from './dashbroad/ManageStudent'
+import ManagePost from './dashbroad/ManagePost'
+import MyProfile from  './dashbroad/MyProfile'
+import ChangePassword from './dashbroad/ChangePassword'
+import AcademicSupervisor from './dashbroad/AcademicSupervisor'
 
+import RegisterStudent from './dashbroad/RegisterStudent';
+import RegisterSupervisor from './dashbroad/RegisterSupervisor';
+import RegisterPost from './dashbroad/RegisterPost';
+import { FieldRequest, PtLacation } from './dashbroad/PtLacation';
+import { StudentRequest } from './dashbroad/StudentRequest';
+import StudentSupervisorList from './dashbroad/SupervisorList';
+import { ManagePosts } from './dashbroad/ManagePosts';
+import ManageRequest from './dashbroad/ManageRequest';
 
 function App() {
   return (
-    <div className="App">
-      {/*  functional component */}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Login />} />
+        <Route path="/pt" element={<PT />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/review" element={<Review />} />
+        <Route path='/Home' element={<Index/>}></Route>
+        <Route path="/managestudent" element={<ManageStudent />} />
+        <Route path="/managepost" element={<ManagePost />} />
+        <Route path="/registerPost" element={<RegisterPost />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/changepassword" element={<ChangePassword />} />
+        <Route path="/academicsupervisor" element={<AcademicSupervisor />} />
+        {/* <Route path="/industrialsupervisor" element={<IndustrialSupervisor />} /> */}
 
-       {/* <FComponent /> */}
+        <Route path='/ptLocation' element={<PtLacation/>}></Route>
 
-       {/* == functional component with props == */}
+        <Route path='/registerStudent' element={<RegisterStudent/>}></Route>
+        <Route path='/registerSupervisor' element={<RegisterSupervisor/>}></Route>
+        <Route path='/editStudent/:id' element={<RegisterStudent/>}></Route>
+        <Route path='/editPost/:id' element={<RegisterPost/>}></Route>
+        <Route path='/editSupervisor/:id' element={<RegisterSupervisor></RegisterSupervisor>}></Route>
 
-      {/* <FComponent name="firstValue" address="Kilima hewa"> 
-        <p>Inside of functional component</p>
-      </FComponent>
-      <FComponent name="SecondValue"/>
-      <FComponent name="thridValue"/> */}
+        <Route path='/addRequest/:id' element={<FieldRequest/>}></Route>
 
-      {/* == class component == */}
-      {/* <CComponent /> */}
+        
+        <Route path='/studentRequest' element={<StudentRequest/>}></Route>
+        <Route path='/supervisorList' element={<StudentSupervisorList/>}></Route>
 
-      {/* == class component with props == */}
+        {/* supervsior */}
+        <Route path='/ManagePosts' element={<ManagePosts/>}></Route>
+        <Route path='/ManageRequest' element={<ManageRequest/>}></Route>
 
-      {/* <CComponent address="First address">
-        <p>Child inside of class component</p>
-      </CComponent>
-
-      <CComponent address="Second address"/>
-      <CComponent address="Third address"/> */}
-
-      {/* <State/> */}
-
-      {/* <Counter/> */}
-
-      {/* <FunctionClick/> */}
-
-      {/* <ClassClick/> */}
-
-      {/* == css style == */}
-
-      {/* <Style/> */}
-
-      {/* <h4 className="error">This is an error </h4>
-      <h4 className={styles.success}>This is sucess msg</h4> */}
-
-      {/* == form == */}
-
-      {/* <Form/> */}
-
-      {/* fragment component  */}
-
-      {/* <Fragment/> */}
-
-      {/* table component */}
-
-      <Table></Table>
-
-
-    </div>
+      </Routes>
+    </Router>
   );
 }
 
